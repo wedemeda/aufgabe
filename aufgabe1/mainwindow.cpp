60b8ffaa715adc4b5f342fd28ddbe0a7ae785309
wedemeda <47379283+wedemeda@users.noreply.github.com>
2019-10-28 21:04:17 +0300

    Add files via upload

--- aufgabe1/mainwindow.cpp
+++ aufgabe1/mainwindow.cpp
@@ -22,45 +22,56 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_button_clicked()
 {
-    double a = ui->kef_a->text().toInt();
-    double b = ui->kef_b->text().toInt();
-    double c = ui->kef_c->text().toInt();
+    double a = ui->kef_a->text().toDouble();
+    double b = ui->kef_b->text().toDouble();
+    double c = ui->kef_c->text().toDouble();
     double d = qPow(b,2) - 4*a*c;
-    if (a==0)
+
+    if ((a!=0.0)&&(b!=0.0)&&(c!=0.0))
+                            {
+                             if (d > 0.0)
+                                       {  double root_1 = (-b + qSqrt(d))/(2*a);
+                                          double root_2 = (-b - qSqrt(d))/(2*a);
+                                          ui->roots->setText("x1 =  "+QString::number(root_1)+"   x2 = "+QString::number(root_2));
+                                       }
+                             if (d == 0.0)
+                                       {  double root_1 = (-b + qSqrt(d))/(2*a);
+                                          ui->roots->setText("x = "+QString::number(root_1));
+                                       }
+                             if (d < 0.0)
+
+                                          ui->roots->setText("Нет корней!");
+                           }
+
+
+      if (a==0.0&&b==0.0&&c==0.0)
+                   { ui->roots->setText("Нет корней!");}
+       else   if (a==0.0&&b==0.0)
+                       { ui->roots->setText("Нет корней!");}
+         else      if ((b==0.0)&&(c==0.0))
+                       { double root_1 = 0;
+                         ui->roots->setText("x = "+QString::number(root_1));
+                       }
+
+      else   if (a==0.0)
              { double root_1 = -(c/b);
                ui->roots->setText("x = "+QString::number(root_1));
              }
-    if (b==0)
-             {  if ((a>0&&c>0)||(a<0&c<0))
-                                                 { ui->roots->setText("Нет корней!");
-                                                 }
-                 else                            { double root_1 = qSqrt(-c/a);
-                                                   double root_2 = -(qSqrt(-c/a));
-                                                   ui->roots->setText("x1 =  "+QString::number(root_1)+"   x2 = "+QString::number(root_2));
-                                                 }
+      else   if (b==0.0)
+             {  if ((a>0.0&&c>0.0)||(a<0.0&&c<0.0))
+                                                { ui->roots->setText("Нет корней!");}
+                else                            { double root_1 = qSqrt(-c/a);
+                                                  double root_2 = -(qSqrt(-c/a));
+                                                  ui->roots->setText("x1 =  "+QString::number(root_1)+"   x2 = "+QString::number(root_2));
+                                                }
              }
-    if (c==0)
+
+
+    else   if (c==0.0)
              {  double root_1 = 0;
                 double root_2 = -(b/a);
                 ui->roots->setText("x1 =  "+QString::number(root_1)+"   x2 = "+QString::number(root_2));
              }
-    if ((b==0)&&(c==0))
-                      { double root_1 = 0;
-                         ui->roots->setText("x = "+QString::number(root_1));
-                      }
-    if ((a!=0)&&(b!=0)&&(c!=0))
-                               {
-                                if (d > 0)
-                                          {  double root_1 = (-b + qSqrt(d))/(2*a);
-                                             double root_2 = (-b - qSqrt(d))/(2*a);
-                                             ui->roots->setText("x1 =  "+QString::number(root_1)+"   x2 = "+QString::number(root_2));
-                                          }
-                                if (d == 0)
-                                          {  double root_1 = (-b + qSqrt(d))/(2*a);
-                                             ui->roots->setText("x = "+QString::number(root_1));
-                                          }
-                                if (d < 0)
 
-                                             ui->roots->setText("Нет корней!");
-                              }
+
 }
