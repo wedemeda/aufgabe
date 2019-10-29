e0159de1cd1697174e471e233bc5e308d797f5d6
wedemeda <47379283+wedemeda@users.noreply.github.com>
2019-10-29 09:54:57 +0300

    Add files via upload

--- aufgabe1_2/mainwindow.cpp
+++ aufgabe1_2/mainwindow.cpp
@@ -19,11 +19,18 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-     double a = ui->side_a->text().toInt();
-     double b = ui->side_b->text().toInt();
-     double angl = ui->anglField->text().toInt();
-     if (ui->gradButton->isChecked()) angl = qDegreesToRadians(angl);
-     double c = qSqrt(a*a+b*b-2*a*b*qCos(angl));
-     ui->result->setText(QString::number(c)+"  метров");
+     double a = ui->side_a->text().toDouble();
+     double b = ui->side_b->text().toDouble();
+     double angl = ui->anglField->text().toDouble();
+     if (a<=0.0||b<=0.0||angl<=0.0)
+        { ui->result->setText("Вводимые значения должны быть > 0!");}
+     else if ((ui->radButton->isChecked())&&(angl>M_PI))
+             {ui->result->setText("Значение угла не может быть > числи пи!");}
+         else if ((angl>180.0))
+                 {ui->result->setText("Значение угла не может быть > 180!");}
+
+    else {if (ui->gradButton->isChecked()) angl = qDegreesToRadians(angl);
+    double c = qSqrt(a*a+b*b-2*a*b*qCos(angl));
+    ui->result->setText(QString::number(c)+"  метров");}
 
 }
